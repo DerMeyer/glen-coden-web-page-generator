4d2d68af30fea9855aa6112e2616588f113cf344
Der Meyer <simon.der.meyer@gmail.com>
2020-04-01 14:28:34 +0200

    Increase headline sizability for mobile.

--- src/components/text/Headline/Headline.js
+++ src/components/text/Headline/Headline.js
@@ -1,9 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
+import Store from '../../../js/Store';
 import { configService } from '../../../index';
-import { i18n } from '../../../js/helpers';
+import { DeviceTypes, OrientationTypes, i18n } from '../../../js/helpers';
 
 
 export default function Headline(props) {
+    const { globalState } = useContext(Store);
     const [config] = useState(() => configService.getComponentConfig(props.id));
 
     const ref = useRef(null);
@@ -27,14 +29,18 @@ export default function Headline(props) {
     }, [config.size]);
 
     useEffect(() => {
+        const sizeFactor = globalState.deviceType === DeviceTypes.MOBILE
+            ? (globalState.orientationType === OrientationTypes.PORTRAIT ? config.mobileSizeBy.portrait : config.mobileSizeBy.landscape)
+            : 1;
         setStyle({
             margin: 0,
             fontWeight: 'bold',
-            fontSize: config.style.fontSizes[element],
+            whiteSpace: 'nowrap',
+            fontSize: config.style.fontSizes[element] * sizeFactor,
             fontFamily: config.style.fontTypes[config.fontTypeIndex] || config.style.fontTypes[0],
             color: config.style.colors[config.color]
         });
-    }, [config, element]);
+    }, [config, globalState.deviceType, globalState.orientationType, element]);
 
     const text = i18n(config.text);
 
