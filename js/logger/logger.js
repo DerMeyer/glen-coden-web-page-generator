fd8e0c254b737918204090945e6791330154ebc2
Der Meyer <simon.der.meyer@gmail.com>
2020-09-03 19:58:06 +0200

    Print, warn, success and error methods in logger.

--- js/logger/logger.js
+++ js/logger/logger.js
@@ -1,8 +1,26 @@
 const chalk = require('chalk');
+const { log } = console;
 
 
-function write(string) {
-    console.log(chalk.green(string));
+function print(string, first = false, last = false) {
+    log(`${first ? '\n' : ''}\t${chalk.green(string)}${last ? '\n' : ''}`);
+}
+function warn(string, first = false, last = false) {
+    log(`${first ? '\n' : ''}\t${chalk.red(string)}${last ? '\n' : ''}`);
 }
 
-exports.write = write;
+function success(string) {
+    log(`\n\t${chalk.green.bgGreen(`\t${string}\t\s`)}`);
+    log(`\t${chalk.bgGreen(' ')}\t${chalk.blue(string)}\t${chalk.bgGreen(' ')}`);
+    log(`\t${chalk.green.bgGreen(`\t${string}\t\s\n`)}`);
+}
+function error(string) {
+    log(`\n\t${chalk.red.bgRed(`\t${string}\t\s`)}`);
+    log(`\t${chalk.bgRed(' ')}\t${chalk.blue(string)}\t${chalk.bgRed(' ')}`);
+    log(`\t${chalk.red.bgRed(`\t${string}\t\s\n`)}`);
+}
+
+exports.print = print;
+exports.warn = warn;
+exports.success = success;
+exports.error = error;
--- project-info.js
+++ project-info.js
@@ -1,4 +1,5 @@
 const logger = require('./js/logger/logger');
 const projectInfo = require('./src/project-info.json');
 
-logger.write(projectInfo.projectName);
+
+logger.print(`Name: ${projectInfo.projectName}`, true, true);
